c750b004058c4735fc66d4cea6dcd2605e078f0b
Alfie Goatcher <aggoatch@amazon.com>
2023-03-06 17:38:12 +0000

    Add footer to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,43 +5,48 @@ export default () => {
   return (
     <>
       <CustomerHead />
-      <div className="m-auto flex flex-col items-center">
-        <h1 className="mb-8 mt-48 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
-          The GraphQL Product Builder
-        </h1>
-        <p className="mb-8 text-center text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
-          The GraphQL Product Builder allows GraphQL schemas to be dynamically
-          applied to each GraphQL customer. A product is built up of GraphQL
-          subsets, once a product has been configured the product can be
-          assigned to as many customers as you want!
-        </p>
-        <div className="flex space-x-4">
-          {[
-            { href: "graphQLSubsets", text: "GraphQL Subsets" },
-            { href: "products", text: "Products" },
-            { href: "customers", text: "Customers" },
-          ].map((page) => {
-            return (
-              <Link
-                className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-                href={page.href}
-              >
-                {page.text}
-                <svg
-                  className="ml-2 -mr-1 h-5 w-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+      <div className="min-h-screen mx-auto flex w-full flex-col px-1 text-gray-700 antialiased">
+        <div className="mw-auto flex flex-col flex-1 items-center">
+          <h1 className="mb-8 mt-48 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+            The GraphQL Product Builder
+          </h1>
+          <p className="mb-8 text-center text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
+            The GraphQL Product Builder allows GraphQL schemas to be dynamically
+            applied to each GraphQL customer. A product is built up of GraphQL
+            subsets, once a product has been configured the product can be
+            assigned to as many customers as you want!
+          </p>
+          <div className="flex space-x-4">
+            {[
+              { href: "graphQLSubsets", text: "GraphQL Subsets" },
+              { href: "products", text: "Products" },
+              { href: "customers", text: "Customers" },
+            ].map((page) => {
+              return (
+                <Link
+                  className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+                  href={page.href}
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </Link>
-            );
-          })}
+                  {page.text}
+                  <svg
+                    className="ml-2 -mr-1 h-5 w-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+        <div className="border-gray-300 py-8 text-center text-sm font-bold">
+          Made by Alfie Goatcher with ❤️
         </div>
       </div>
     </>
